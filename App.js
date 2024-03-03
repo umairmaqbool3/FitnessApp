701609d821a23bc5a66168716a627031e5c3351e
Umair Maqbool <umair.maqbool55@gmail.com>
2024-03-03 10:41:21 +0500

    Structure created for app

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AppNavigation from './src/navigations/AppNavigation'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { ThemeProvider } from './src/theme/ThemeProvider'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+       <SafeAreaProvider>
+            <AppNavigation />
+      </SafeAreaProvider>
+    </ThemeProvider>
   );
 }
 
